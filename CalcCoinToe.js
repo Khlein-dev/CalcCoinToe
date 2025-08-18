583f55eea87a5ebe8631a7a83ddaa511d7e5a894
Khlein-dev <kierjerymiepascua@gmail.com>
2025-08-18 19:40:53 +0800

    wth Containers

--- CalcCoinToe.js
+++ CalcCoinToe.js
@@ -2,3 +2,10 @@ function  cal() {
     document.querySelector(`body`).classList.add(`showCal`)
 }
 
+function  con() {
+    document.querySelector(`body`).classList.add(`showCon`)
+}
+
+function tik() {
+    document.querySelector(`body`).classList.add(`showTik`)
+}
