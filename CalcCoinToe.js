759be4ffb7c05ed0ce71e62470e63d79db8cc0df
Khlein-dev <kierjerymiepascua@gmail.com>
2025-08-19 20:30:17 +0800

    wth functioning ext btns

--- CalcCoinToe.js
+++ CalcCoinToe.js
@@ -1,11 +1,38 @@
-function  cal() {
+function cal() {
     document.querySelector(`body`).classList.add(`showCal`)
+    const calculator = document.querySelector('.calculate');
+    calculator.style.display = 'block';
+
 }
 
-function  con() {
+function con() {
     document.querySelector(`body`).classList.add(`showCon`)
+    const convert = document.querySelector('.convert');
+    convert.style.display = 'block';
 }
 
 function tik() {
     document.querySelector(`body`).classList.add(`showTik`)
+    const tictac = document.querySelector('.tictac');
+    tictac.style.display = 'block';
 }
+
+const calculator = document.querySelector('.calculate');
+const convert = document.querySelector('.convert');
+const tictac = document.querySelector('.tictac');
+const exitBtn = document.querySelector('.exitCalc');
+const exitConBtn = document.querySelector('.exitCon');
+const exitTicBtn = document.querySelector('.exitTic');
+
+
+exitBtn.addEventListener('click', () => {
+    calculator.style.display = 'none';
+});
+
+exitConBtn.addEventListener('click', () => {
+    convert.style.display = 'none';
+});
+
+exitTicBtn.addEventListener('click', () => {
+    tictac.style.display = 'none';
+}); 
