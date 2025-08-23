f382fd02f9b1c23af389d901c203fc62ef682d3b
Khlein-dev <kierjerymiepascua@gmail.com>
2025-08-23 16:49:05 +0800

    TicTacToe with Js functions

--- CalcCoinToe.js
+++ CalcCoinToe.js
@@ -116,6 +116,54 @@ convertBtn.addEventListener("click", () => {
     resultDiv.textContent = `${amount} in base currency = ${converted.toFixed(2)} ${selectedCurrency}`;
 });
 
+// Tic Tac Toe
+
+let board = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let gameActive = true;
+const statusMessage = document.getElementById("statusMessage");
+
+function makeMove(index) {
+    if (board[index] === "" && gameActive) {
+        board[index] = currentPlayer;
+        document.getElementsByClassName("cell")[index].innerText = currentPlayer;
+        if (checkWinner()) {
+            statusMessage.innerText = `Player ${currentPlayer} wins!`;
+            gameActive = false;
+        } else if (board.every(cell => cell !== "")) {
+            statusMessage.innerText = "It's a draw!";
+            gameActive = false;
+        } else {
+            currentPlayer = currentPlayer === "X" ? "O" : "X";
+            statusMessage.innerText = `Player ${currentPlayer}'s turn`;
+        }
+    }
+}
+
+function checkWinner() {
+    const winningCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]             // Diagonals
+    ];
+    return winningCombinations.some(combination => {
+        const [a, b, c] = combination;
+        return board[a] && board[a] === board[b] && board[a] === board[c];
+    });
+}
+
+function resetGame() {
+    board = ["", "", "", "", "", "", "", "", ""];
+    currentPlayer = "X";
+    gameActive = true;
+    statusMessage.innerText = `Player ${currentPlayer}'s turn`;
+    const cells = document.getElementsByClassName("cell");
+    for (let cell of cells) {
+        cell.innerText = "";
+    }
+}
+
+
 
 
 
